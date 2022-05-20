699b4e80d99962b90e435073d87d3d3aeb4ca55f
Anthony <anthonymellon@yahoo.com>
2022-05-20 15:36:13 +1200

    memaw spirte sheet included and used. players sorting layer now depends on y position

--- GhostDouku/Assets/Scripts/PlayerMovement.cs
+++ GhostDouku/Assets/Scripts/PlayerMovement.cs
@@ -12,8 +12,14 @@ public class PlayerMovement : MonoBehaviour {
 
     private Vector3 target;
 
+    public Texture2D idleLeft;
+
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
     void Start() {
         target = transform.position;
+        print(idleLeft);
     }
 
     // Update is called once per frame
@@ -32,6 +38,25 @@ public class PlayerMovement : MonoBehaviour {
             heldDownTimer = 0;
         }
         transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        print(target);
+
+        //Animation control
         
+        if(transform.position.x > target.x) //Moving left
+        {
+            animator.SetBool("moving", true);
+            animator.SetBool("facingLeft", true);
+        }
+        if(transform.position.x < target.x) //Moving right
+        {
+            animator.SetBool("moving", true);
+            animator.SetBool("facingLeft", false);
+        }
+        if(transform.position.x == target.x) //Not moving
+        {
+            animator.SetBool("moving", false);
+        }
+
+        spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y) * -1;
     }
 }
