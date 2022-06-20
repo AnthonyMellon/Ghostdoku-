eba7e95b4dccc960618afe218e374ff3fe3c2286
smitkb1 <smitkb1@student.op.ac.nz>
2022-06-20 18:40:46 +1200

    Add volume setting to Hub pause menu

--- GhostDouku/Assets/Scripts/Music.cs
+++ GhostDouku/Assets/Scripts/Music.cs
@@ -9,33 +9,8 @@ public class Music : MonoBehaviour
 
     void Start()
     {
-        if (!PlayerPrefs.HasKey("musicVolume"))
-        {
-            PlayerPrefs.SetFloat("musicVolume", 1);
-            VolumeLoad();
-        }
-        else
-        {
-            VolumeLoad();
-        }
         DontDestroyOnLoad(transform.gameObject);     
     }
 
-    public void ChangeVolume()
-    {
-        AudioListener.volume = volumeSlider.value;
-        VolumeSave();
-    }
-
-    public void VolumeLoad()
-    {
-        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
-        AudioListener.volume = volumeSlider.value;
-    }
-
-    private void VolumeSave()
-    {
-        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
-    }
 
 }
